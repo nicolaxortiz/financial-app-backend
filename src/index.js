6cc31602207259bb4243442822ec54d777f8ba64
nicolaxortiz <nicolaxortiz@gmail.com>
2025-12-18 16:25:19 -0500

    change corsOption

--- src/index.js
+++ src/index.js
@@ -8,27 +8,17 @@ import codesRouter from "./routes/codes.routes.js";
 
 const app = express();
 
-const corsOptions = {
-  origin: process.env.ORIGIN_ENV,
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-type, Accept, Access-Control-Allow-Request-Method"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
-app.use(cors(corsOptions));
 app.use("/api/users", userRouter);
 app.use("/api/accounts", accountRouter);
 app.use("/api/moves", movesRouter);
